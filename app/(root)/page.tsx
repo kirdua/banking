7b4cc8f0f7a83e101a77ddffd27a3a845b645e52
kirdua <kirdua@gmail.com>
2024-05-31 14:32:57 -0500

    implement headerbox totalbalance doughnut animateed counter

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,31 @@
+import TotalBalanceBox from '@/components/TotalBalanceBox'
+import HeaderBox from '@/components/HeaderBox'
 import React from 'react'
 
 const Home = () => {
-  return <div>Home</div>
+  const isLoggedIn = {
+    firstName: 'Kirby',
+  }
+
+  return (
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+          <HeaderBox
+            type='greeting'
+            title='Welcome'
+            user={isLoggedIn?.firstName || 'Guest'}
+            subtext='Access and manage your account and transactions efficiently.'
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+      </div>
+    </section>
+  )
 }
 
 export default Home
