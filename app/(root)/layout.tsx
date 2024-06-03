b1b204753cf460cbf58ebcfd743cb653fd965c79
kirdua <kirdua@gmail.com>
2024-06-03 09:13:51 -0500

    implement sidbar basics

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,18 @@
+import Sidebar from '@/components/Sidebar'
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode
 }>) {
+  const loggedIn = {
+    firstName: 'Kirby',
+    lastName: 'Craft',
+  }
+
   return (
-    <main>
-      SIDEBAR
+    <main className='flex h-screen w-full font-inter'>
+      <Sidebar user={loggedIn} />
       {children}
     </main>
   )
